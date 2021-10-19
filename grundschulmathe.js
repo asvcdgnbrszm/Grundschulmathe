78631d8a798649c3c7f81ef0119ba4705039eb99
asvcdgnbrszm <n1ckel@web.de>
2021-10-19 22:38:49 +0200

    fullfills task b

--- grundschulmathe.js
+++ grundschulmathe.js
@@ -1,1 +1,10 @@
-alert("Grundschulmathe")
+var a = Math.floor(Math.random() * 20)
+var b = Math.floor(Math.random() * 20)
+
+var result = prompt(`Wieviel ist ${a} + ${b}?`)
+
+if (parseInt(result, 10) === a + b) {
+    alert("Richtig!")
+} else {
+    alert("Falsch!")
+}
