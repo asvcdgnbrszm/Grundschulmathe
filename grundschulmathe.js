d68e6d49e4e592840246d5606ba9a70eabd896e8
asvcdgnbrszm <n1ckel@web.de>
2022-01-12 21:18:45 +0100

    improved after feedback

--- grundschulmathe.js
+++ grundschulmathe.js
@@ -1,53 +1,56 @@
-var count = 0
-var lost = false
-var level = 1
+var rightAnswerCount = 0
+var end = false
+var level = 0
 
-const obj = {
-    1: function(a,b) {return [a+b, `Wieviel ist ${a} + ${b}?`]},
-    2: function(a,b) {if (a>b){return [a-b, `Wieviel ist ${a} - ${b}?`]}else{return [b-a, `Wieviel ist ${b} - ${a}?`]} },
-    3: function(a,b) {return [a*b, `Wieviel ist ${a} * ${b}?`]},
-    4: function(a,b) {return [a*b/a, `Wieviel ist ${a*b} / ${a}?`]}
+const levels = [
+    function(a,b) {return {equation: a+b, question: `Wieviel ist ${a} + ${b}?`}},
+    function(a,b) {if (a>b){return {equation: a-b, question: `Wieviel ist ${a} - ${b}?`}}else{return {equation: b-a, question: `Wieviel ist ${b} - ${a}?`}}},
+    function(a,b) {return {equation: a*b, question: `Wieviel ist ${a} * ${b}?`}},
+    function(a,b) {return {equation: a*b/a, question: `Wieviel ist ${a*b} / ${a}?`}},
+]
+
+
+while(!end){
+    MathTest(levels[level]);
 }
 
 
-while (!lost){
+function MathTest(func) {
 
     for(let i = 0; i < 5; i++){
-
         var a = Math.floor(Math.random() * 20)
         var b = Math.floor(Math.random() * 20)
+
+        var result = prompt(func(a,b).question)
     
-        var result = prompt(obj[level](a,b)[1])
-    
-        if (parseInt(result, 10) === obj[level](a,b)[0]) {
-            count++
-            alert(`Richtig! (${count}/5 richtig)`)
+        if (parseInt(result, 10) === func(a,b).equation) {
+            rightAnswerCount++
+            alert(`Richtig! (${rightAnswerCount}/5 richtig)`)
         } else {
-            alert(`Falsch! (${count}/5 richtig)`)
+            alert(`Falsch! (${rightAnswerCount}/5 richtig)`)
         }
     }
     
-    if (level < Object.keys(obj).length) {
-        if (count >= 4) {
+    if (level < levels.length-1) {
+        if (rightAnswerCount >= 4) {
             level++
-            count = 0
+            rightAnswerCount = 0
             alert(`Glückwunsch! 
             Auf ins nächste Level!`)
         }else{
-            lost = true
+            end = true
             alert(`Für das nächste Level brauchst du 4 richtige!
             Versuche es noch einmal!`)
         } 
     }else{
-        if (count >= 4) {
-            lost = true
+        if (rightAnswerCount >= 4) {
+            end = true
             alert(`Glückwunsch! 
             Alle Level bestanden!`)
         }else{
-            lost = true
+            end = true
             alert(`Du hast das letzte Level leider nicht geschafft!
             Versuche es noch einmal!`)
         } 
     }    
 }
-
